bb2e3150499dafd6f1784e72713ec59550f166c7
Guilherme Nascimento <brcontainer@yahoo.com.br>
2018-05-09 12:14:48 -0300

    Initial

---
+++ jot.js
@@ -0,0 +1,45 @@
+/*
+ * jot.js 0.0.1
+ *
+ * Copyright (c) 2018 Guilherme Nascimento (brcontainer@yahoo.com.br)
+ *
+ * Released under the MIT license
+ */
+
+(function (d, w, u) {
+    "use strict";
+
+    var isReady = false;
+
+    w.Jot = {
+        'qsa': function (query, context) {
+            return (context || document).querySelector(query);
+        },
+        'qs': function (query, context) {
+            return (context || document).querySelectorAll(query);
+        },
+        'ready': function (callback) {
+            if (isReady) {
+                callback();
+                return;
+            }
+
+            var trigged = false;
+
+            function trigger()
+            {
+                if (trigged) return;
+
+                isReady = trigged = true;
+                callback();
+            }
+
+            if (/complete|interactive/i.test(d.readyState)) {
+                trigger();
+            } else {
+                d.addEventListener('DOMContentLoaded', trigger);
+                d.addEventListener('load', trigger);
+            }
+        }
+    };
+})(document, window);
